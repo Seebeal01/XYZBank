bd6b4d2559d717ea4214f3bf4159ce2f2cc7dfbb
Seebeal01 <seebealabulila@gmail.com>
2024-09-20 15:13:53 +0300

    update

--- src/Xyz.java
+++ src/Xyz.java
@@ -1,3 +1,4 @@
+import java.time.Duration;
 import java.util.List;
 import java.util.Random;
 
@@ -11,26 +12,26 @@ import org.testng.annotations.Test;
 public class Xyz {
 	WebDriver driver = new EdgeDriver();
 	String Url = "https://globalsqa.com/angularJs-protractor/BankingProject/#/login";
+	Random random = new Random();
 
 	@BeforeTest
 	public void mystup() {
 		driver.manage().window().maximize();
 		driver.get(Url);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 	}
 
 	@Test(priority = 1)
-	public void logINCustumer() throws InterruptedException {
-		Thread.sleep(5000);
+	public void logINCustumer() {
+
 		WebElement customerLoginButton = driver.findElement(By.cssSelector("button[ng-click='customer()']"));
 		customerLoginButton.click();
 	}
 
 	@Test(priority = 2)
-	public void SelectCustomer() throws InterruptedException {
-		Thread.sleep(5000);
+	public void SelectCustomer() {
 
 		List<WebElement> customerOptions = driver.findElements(By.className("ng-binding"));
-		Random random = new Random();
 		int Customer = customerOptions.size();
 		int randomcustomer = random.nextInt(Customer);
 		customerOptions.get(randomcustomer).click();
@@ -38,12 +39,13 @@ public class Xyz {
 		WebElement loginButton = driver.findElement(By.cssSelector("button[type='submit']"));
 		loginButton.click();
 	}
+
 	@Test(priority = 3)
-	public void Deposit() throws InterruptedException {
-		Thread.sleep(5000);
+	public void Deposit() {
 		driver.findElement(By.xpath("//button[normalize-space()='Deposit']")).click();
-		
-		
+		int randomnumber = random.nextInt(99999);
+		driver.findElement(By.xpath("//input[@placeholder='amount']")).sendKeys(String.valueOf(randomnumber));
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
 	}
 
 }
